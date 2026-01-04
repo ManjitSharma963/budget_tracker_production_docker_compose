a354b6afa02c273b7eab9f235977d0777cb34bbd
ManjitSharma963 <Itsmanjitsharma@gmail.com>
2026-01-04 20:27:41 +0530

    first commit

---
+++ mysql/init.sql
@@ -0,0 +1,10 @@
+-- Initialize database schema
+-- This file will be executed when the container is first created
+
+CREATE DATABASE IF NOT EXISTS expenes_tracker;
+
+USE expenes_tracker;
+
+-- Database will be created automatically by Spring Boot
+-- Spring Boot will handle schema creation via JPA/Hibernate
+
